57144f8cf1b2e070d1a29ad9cbca2be1c5544036
Mikkel Malmberg <mikkel@brnbw.com>
2026-03-01 23:35:07 +0100

    Update tuna cask to 0.36 (842)

--- Casks/tuna.rb
+++ Casks/tuna.rb
@@ -1,8 +1,8 @@
 cask("tuna") do
-  version("0.35")
-  sha256("5ab8fa890ba9f9ab549b1f3afece9bb5e5e78296a18923ce69916181b061123d")
+  version("0.36")
+  sha256("7f81a62f060fd3b074aa373dec3345c98942d7a95d59ffb342e4fff133455b16")
 
-  url("https://tunaformac.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsiZGF0YSI6NjYsInB1ciI6ImJsb2JfaWQifX0=--b0efc0c938ce457e17f2f6516bfe27bda5e173ce/Tuna-0.35-818.zip")
+  url("https://tunaformac.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsiZGF0YSI6NzIsInB1ciI6ImJsb2JfaWQifX0=--ff732ae8755c4384dd67ee1cfdc239b7c0e408ff/Tuna-0.36-842.zip")
   name("Tuna")
   desc("Modern launcher")
   homepage("https://tunaformac.com/")
