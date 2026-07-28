893df0698abcb9cde1a4d4c575bcd7f3721a3dcd
Mikkel Malmberg <mikkel@brnbw.com>
2026-07-28 17:22:19 +0200

    Update tuna cask to 0.80 (1798)

--- Casks/tuna.rb
+++ Casks/tuna.rb
@@ -1,17 +1,19 @@
 cask("tuna") do
-  version("0.78")
-  sha256("fb378368d34f6d419bf908727831dbb5a68a497890d857c87f5b692432fc0970")
+  version("0.80")
+  sha256("6fb754c3468edfc090914ab1279fc5b569aa35772f192cca66d0f07d924cd214")
 
-  url("https://tunaformac.com/download/releases/1563")
+  url("https://tunaformac.com/download/releases/1798")
   name("Tuna")
   desc("Modern launcher")
   homepage("https://tunaformac.com/")
-  auto_updates(true)
 
   livecheck do
     url("https://tunaformac.com/appcast.xml")
     strategy(:sparkle, &:short_version)
   end
+
+  auto_updates true
+  depends_on macos: :sequoia
 
   app("Tuna.app")
 
