91ce719e29bd7041aa59628e8103c11c0664a462
Mikkel Malmberg <mikkel@brnbw.com>
2026-09-04 14:32:06 +0200

    Update flick-ring to 1.3.1 (141)

--- Casks/flick-ring.rb
+++ Casks/flick-ring.rb
@@ -1,6 +1,6 @@
 cask("flick-ring") do
-  version("1.3.0")
-  sha256("21a3a7f4fb8204cbd827d14563855d8b39bf7e26650f316d5fa338810af55be9")
+  version("1.3.1")
+  sha256("d0a278f678876de287679c516547c57eae478929b2ed6121d8d261bf30f3a64f")
 
   url(
     "https://github.com/mikker/FlickRing/releases/download/v#{version}/FlickRing.app.zip",
