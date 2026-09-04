18547127226a903a32694b0d5f24929bd245da99
Mikkel Malmberg <mikkel@brnbw.com>
2026-09-04 11:10:20 +0200

    Update tuna cask to 0.96 (2096)

--- Casks/tuna.rb
+++ Casks/tuna.rb
@@ -1,8 +1,8 @@
 cask("tuna") do
-  version("0.95")
-  sha256("e31caf56cba0f07d8f603010282fbdc827cf1c2e559a44fbcb4bb1ca0b4496e3")
+  version("0.96")
+  sha256("3123f29b58343df674ffcc4c6148326887e3fecfb8495555d67e73f78197fb55")
 
-  url("https://tunaformac.com/download/releases/2070")
+  url("https://tunaformac.com/download/releases/2096")
   name("Tuna")
   desc("Modern launcher")
   homepage("https://tunaformac.com/")
